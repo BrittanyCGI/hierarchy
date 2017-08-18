1bda2570638fb5f8d99ca13c9db9f16cbdc2d224
CGI-PRODUCTION <cgi-production@CGI-PRODUCTIONs-iMac.local>
2017-08-18 09:43:41 -0400

    change default tree

--- wp-content/themes/cgidirectory/page-templates/page-hierarchy.php
+++ wp-content/themes/cgidirectory/page-templates/page-hierarchy.php
@@ -16,7 +16,7 @@ get_header(); ?>
 
 				<div class="row">
 					<div class="page-title col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 ceo">
-						<div class="hierarchy-button cgi" id="CGI">
+						<div class="hierarchy-button cgi current" id="CGI">
 							<h1 class="">Bob Bartosiewicz</h1>
 							<h3>CEO & Chairman of the Board</h3>
 						</div>
@@ -24,7 +24,7 @@ get_header(); ?>
 				</div>
 				<div class="row ell-nxt">
 					<div class="page-title col-xs-6 col-md-5 col-md-offset-1 ">
-						<div class="hierarchy-button ell current" id="ELL">
+						<div class="hierarchy-button ell" id="ELL">
 							<h2>Bob Forys</h2>
 							<h3>President | eLocalLink</h3>
 						</div>
@@ -33,14 +33,14 @@ get_header(); ?>
 
 
 					<div class="page-title col-xs-6 col-md-5">
-					<div class="nxt hierarchy-button" id="NXT">
-						<h2>Frank Buono</h2>
-						<h3>President | Next! Ad Agency</h3>
+						<div class="nxt hierarchy-button" id="NXT">
+							<h2>Frank Buono</h2>
+							<h3>President | Next! Ad Agency</h3>
+						</div>
 					</div>
-				</div>
 			</header><!-- .page-header -->
 
-			<div class="tree col-md-12 hidden" id="CGI-tree">
+			<div class="tree col-md-12 shown" id="CGI-tree">
 				<ul class="tree-ul">
 
 					<?php get_template_part( 'template-parts/content-cgi' ); ?>
@@ -53,7 +53,7 @@ get_header(); ?>
 
 
 
-			<div class="tree col-md-12 shown" id="ELL-tree">
+			<div class="tree col-md-12 hidden" id="ELL-tree">
 				<ul class="tree-ul">
 
 					<?php get_template_part( 'template-parts/content-ell' ); ?>
